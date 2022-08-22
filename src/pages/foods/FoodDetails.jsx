f209ff94b26660ac13571b45da41e0c13428f960
Gabriel Vinícius <viniciusgsa99@gmail.com>
2022-08-22 18:33:06 -0300

    Requisito 24 concluído. Implementando fetch para API da tela de detalhes de bebibas/comidas.

--- src/pages/foods/FoodDetails.jsx
+++ src/pages/foods/FoodDetails.jsx
@@ -1,5 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import fetchRecipe from '../../services/fetchRecipe';
 
 export default function FoodDetails() {
-  return (<h1>FoodDetails</h1>);
+  // const [myFood, setMyFood] = useState();
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchMeal = async () => {
+      console.log(await fetchRecipe('themealdb', id));
+    };
+    fetchMeal();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div>
+      Foods
+    </div>
+  );
 }
