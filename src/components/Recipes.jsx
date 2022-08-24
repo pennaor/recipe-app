f6fc5c7b50be5ad08934273e3e52d53f23b9ac34
jcaluiz <lacjunior129.la@gmail.com>
2022-08-24 16:39:48 -0300

    requisitos 20, 21 e 22 concluídos

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -1,15 +1,17 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import RecipeContext from '../context/RecipeContext';
+import { fetchRecipeAll } from '../services/fetchRecipe';
 
 export default function Recipes() {
-  const { meals, drinks, searchClick,
+  const {
     foodsDrinksRecipes, setFoodsDrinksRecipes } = useContext(RecipeContext);
+  const [buttonClickCategory, setButtonClickCategory] = useState();
+  const [buttonAll, setButtonAll] = useState(true);
 
   const { pathname } = useHistory().location;
   const TWELVE = 12;
   const FIVE = 5;
-  const recipesElement = (pathname === '/foods' ? meals : drinks);
   const dataTestidElement = (pathname === '/foods' ? 'Meal' : 'Drink');
   const buttonList = (pathname === '/foods' ? foodsDrinksRecipes
   && foodsDrinksRecipes['2'].meals
@@ -18,28 +20,67 @@ export default function Recipes() {
       .filter((_category, index) => index < FIVE));
 
   const fetchAPIDrinkFood = async () => {
-    const urlDrink = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
-    const urlFood = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
-    const urlFoodCategoryList = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
-    const urlDrinkCategoryList = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
-    const responseDrink = await fetch(urlDrink).then((data) => data.json());
-    const responseFood = await fetch(urlFood).then((data) => data.json());
-    const responseFoodCategoryList = await fetch(urlFoodCategoryList)
-      .then((data) => data.json());
-    const responseDrinkCategoryList = await fetch(urlDrinkCategoryList)
-      .then((data) => data.json());
-    setFoodsDrinksRecipes([responseFood, responseDrink,
-      responseFoodCategoryList, responseDrinkCategoryList]);
+    try {
+      const urlDrink = fetchRecipeAll('thecocktaildb', 'all');
+      const urlFood = fetchRecipeAll('themealdb', 'all');
+      const urlFoodCategoryList = fetchRecipeAll('themealdb', 'categories');
+      const urlDrinkCategoryList = fetchRecipeAll('thecocktaildb', 'categories');
+      const responseDrink = await fetch(urlDrink).then((data) => data.json());
+      const responseFood = await fetch(urlFood).then((data) => data.json());
+      const responseFoodCategoryList = await fetch(urlFoodCategoryList)
+        .then((data) => data.json());
+      const responseDrinkCategoryList = await fetch(urlDrinkCategoryList)
+        .then((data) => data.json());
+      setFoodsDrinksRecipes([responseFood, responseDrink,
+        responseFoodCategoryList, responseDrinkCategoryList]);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  console.log(foodsDrinksRecipes);
-  console.log(searchClick);
-  console.log(buttonList);
+  const [foodsDrinksCategories, setFoodsDrinksCategories] = useState();
+
+  const fetchAPIByCategories = async (category, path) => {
+    try {
+      const urlFilterByCategoriesFood = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`;
+      const urlFilterByCategoriesDrink = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`;
+      const response = await fetch(path === '/foods'
+        ? urlFilterByCategoriesFood : urlFilterByCategoriesDrink)
+        .then((data) => data.json());
+      setFoodsDrinksCategories(response);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
     fetchAPIDrinkFood();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  useEffect(() => {
+    if (buttonClickCategory === '') {
+      setButtonAll(true);
+    }
+    if (buttonClickCategory !== '' && buttonClickCategory) {
+      setButtonAll(false);
+    }
+    fetchAPIByCategories(buttonClickCategory, pathname);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [buttonClickCategory]);
+
+  const listToRender = () => (pathname === '/foods'
+    ? foodsDrinksRecipes && (buttonAll
+      ? foodsDrinksRecipes['0'].meals
+      : foodsDrinksCategories && (foodsDrinksCategories.meals
+        || foodsDrinksRecipes['0'].meals))
+    : foodsDrinksRecipes && (buttonAll
+      ? foodsDrinksRecipes['1'].drinks
+      : foodsDrinksCategories && (foodsDrinksCategories.drinks
+            || foodsDrinksRecipes['1'].drinks)));
+
+  const render = listToRender();
 
   return (
     <div>
@@ -50,68 +91,48 @@ export default function Recipes() {
               type="button"
               data-testid={ `${button.strCategory}-category-filter` }
               key={ button.strCategory }
+              onClick={ () => {
+                if (buttonClickCategory === button.strCategory) {
+                  setButtonClickCategory('');
+                  return;
+                }
+                setButtonClickCategory(button.strCategory);
+              } }
             >
               {button.strCategory}
 
             </button>
           ))
         }
+        <button
+          type="button"
+          onClick={ () => setButtonAll(true) }
+          data-testid="All-category-filter"
+        >
+          All
+        </button>
+
       </div>
-      { (meals.length > 0 || drinks.length > 0)
-      && recipesElement
-        .map((meal, index) => (index < TWELVE) && (
-          <div
-            data-testid={ `${index}-recipe-card` }
-            key={ meal[`str${dataTestidElement}Thumb`] }
-          >
-            <img
-              src={ meal[`str${dataTestidElement}Thumb`] }
-              alt={ meal[`str${dataTestidElement}Thumb}`] }
-              data-testid={ `${index}-card-img` }
-            />
-            <p data-testid={ `${index}-card-name` }>{meal[`str${dataTestidElement}`]}</p>
-          </div>
-        ))}
       {
-        !searchClick
-        && (pathname === '/foods'
-          ? foodsDrinksRecipes && foodsDrinksRecipes['0'].meals
-            .map((meal, index) => (index < TWELVE) && (
-              <div
-                data-testid={ `${index}-recipe-card` }
-                key={ meal[`str${dataTestidElement}Thumb`] }
+        (render && render
+          .map((meal, index) => (index < TWELVE) && (
+            <div
+              data-testid={ `${index}-recipe-card` }
+              key={ meal[`str${dataTestidElement}Thumb`] }
+            >
+              <img
+                src={ meal[`str${dataTestidElement}Thumb`] }
+                alt={ meal[`str${dataTestidElement}Thumb}`] }
+                data-testid={ `${index}-card-img` }
+              />
+              <p
+                data-testid={ `${index}-card-name` }
               >
-                <img
-                  src={ meal[`str${dataTestidElement}Thumb`] }
-                  alt={ meal[`str${dataTestidElement}Thumb}`] }
-                  data-testid={ `${index}-card-img` }
-                />
-                <p
-                  data-testid={ `${index}-card-name` }
-                >
-                  {meal[`str${dataTestidElement}`]}
+                {meal[`str${dataTestidElement}`]}
 
-                </p>
-              </div>
-            )) : foodsDrinksRecipes && foodsDrinksRecipes['1'].drinks
-            .map((drink, index) => (index < TWELVE) && (
-              <div
-                data-testid={ `${index}-recipe-card` }
-                key={ drink[`str${dataTestidElement}Thumb`] }
-              >
-                <img
-                  src={ drink[`str${dataTestidElement}Thumb`] }
-                  alt={ drink[`str${dataTestidElement}Thumb}`] }
-                  data-testid={ `${index}-card-img` }
-                />
-                <p
-                  data-testid={ `${index}-card-name` }
-                >
-                  {drink[`str${dataTestidElement}`]}
-
-                </p>
-              </div>
-            )))
+              </p>
+            </div>
+          )))
       }
     </div>
   );
--- src/services/fetchRecipe.js
+++ src/services/fetchRecipe.js
@@ -20,8 +20,20 @@ export default function fetchRecipe(api, query, filter) {
   case 'firstLetter':
     url = `${url}/search.php?f=${query}`;
     break;
+  case 'filterByCategory':
+    url = `${url}/search.php?c=${query}`;
+    break;
   default:
     url = `${url}/lookup.php?i=${query}`;
   }
   return fetchAPI(url);
+}
+
+export function fetchRecipeAll(api, filter) {
+  const url = `https://www.${api}.com/api/json/v1/1`;
+  const verificate = {
+    all: `${url}/search.php?s=`,
+    categories: `${url}/list.php?c=list`,
+  };
+  return verificate[filter];
 }
