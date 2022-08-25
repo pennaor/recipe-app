b4993b687b633bb5e65e8a8624a9a9d5cb274609
alexandre rangel <alexandrerangel111@gmail.com>
2022-08-24 21:28:50 -0300

    iniciando pagina RecipeInProgess

---
+++ src/pages/RecipeInProgress.js
@@ -0,0 +1,71 @@
+import React from 'react';
+
+import shareIcon from '../images/shareIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+
+function RecipeInProgress() {
+  return (
+    <div>
+      <img
+        src={ foto da receita }
+        alt="foto da receita finalizada"
+        data-testid="recipe-photo"
+      />
+      <h1
+        data-testid="recipe-title"
+      >
+       { Nome da receita }
+      </h1>
+
+      <button
+        type="button"
+        data-testid="share-btn"
+        onClick={ ação de compartilhar }
+      >
+        <img
+          src={ shareIcon }
+          alt="iconde de compartilhar"
+        />
+      </button>
+
+      <button
+        type="button"
+        data-testid="favorite-btn"
+        onClick={ ação de favoritar }
+      >
+        <img
+          src={ whiteHeartIcon }
+          alt="icone de favoritar"
+        />
+      </button>
+
+      <h4 data-testid="recipe-category">Texto da cetegoria</h4>
+
+      <p>Ingredients</p>
+
+      { Fazer um map e imprimir na lista abaixo }
+      <li>
+        <input
+          data-testid={ `${ind}-ingredient-step` }
+          type="checkbox"
+          name="mudarDepois"
+          value="mudarDepois"
+          onChange={ ação de riscar o item listado }
+        />
+      </li>
+
+      <p data-testid="instructions">
+        { Instruções da receita renderizada }
+      </p>
+
+      <button
+        type="button"
+        data-testid="finish-recipe-btn"
+      >
+        Finish Recipe
+      </button>
+    </div>
+  );
+}
+
+export default RecipeInProgress;
