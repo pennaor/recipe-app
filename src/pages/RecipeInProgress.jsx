8a71c8132f2cd54139de7b1abc048bd4fb054698
alexandre rangel <alexandrerangel111@gmail.com>
2022-08-27 15:14:06 -0300

    concluindo rend dos ingredientes e btn share

--- src/pages/RecipeInProgress.jsx
+++ src/pages/RecipeInProgress.jsx
@@ -1,47 +1,111 @@
 import React, { useContext, useState, useEffect } from 'react';
-import { useParams, useHistory } from 'react-router-dom';
+import { useRouteMatch, useHistory } from 'react-router-dom';
 
 import RecipeContext from '../context/RecipeContext';
 import shareIcon from '../images/shareIcon.svg';
 import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 
-function RecipeInProgress() {
-  const [ingredients, setIngradients] = useState([]);
-  const [informationFood, setInformationFood] = useState([]);
+import fetchRecipe from '../services/fetchRecipe';
+import linkCopied from '../utils/linkCopied';
 
-  const { id } = useParams();
+function RecipeInProgress() {
+  const [ingredients, setIngredients] = useState([]);
+  const [informationFood, setInformationFood] = useState({});
+  const [copied, setCopied] = useState(false);
+
+  const { params: { id }, url } = useRouteMatch();
   const { inProgressRecipes } = useContext(RecipeContext);
   const history = useHistory();
-  const typeRecipe = history.location.pathname.includes('foods') ? 'meals' : 'cocktails';
+  const typeRecipe = history.location.pathname.includes('foods') ? 'themealdb'
+    : 'thecocktaildb';
+
+  function getIngredients(obj) {
+    const keysIngred = Object.keys(obj).filter((e) => e.includes('strIngredient'));
+    const keysMeasures = Object.keys(obj).filter((e) => e.includes('strMeasure'));
+
+    const ingredientsObj = keysIngred.map((key) => obj[key]);
+    const measuresObj = keysMeasures.map((key) => obj[key]);
+
+    let ingredFiltred = {};
+    let measuresFiltred = {};
+    if (typeRecipe === 'themealdb') {
+      ingredFiltred = ingredientsObj.filter((e) => e.length > 0);
+      measuresFiltred = measuresObj.filter((e) => e.length > 0);
+    } else {
+      ingredFiltred = ingredientsObj.filter((e) => e !== null);
+      measuresFiltred = measuresObj.filter((e) => e !== null);
+    }
+
+    const ingredMeas = ingredFiltred.map((e, i) => e = `${e} - ${measuresFiltred[i]}`);
+    setIngredients(ingredMeas);
+
+    /* console.log(ingredFiltred);
+    console.log(measuresFiltred);
+    console.log(ingredMeas); */
+  }
+
+  const getInformationsMeals = async () => {
+    const typeFood = 'meals';
+    const resultArray = await fetchRecipe(typeRecipe, id);
+    const resultObj = resultArray[typeFood][0];
+    console.log(resultObj);
+    setInformationFood({
+      image: resultObj.strMealThumb,
+      name: resultObj.strMeal,
+      category: resultObj.strCategory,
+      instructions: resultObj.strInstructions,
+    });
+    getIngredients(resultObj);
+  };
+
+  const getInformationsDrinks = async () => {
+    const typeFood = 'drinks';
+    const resultArray = await fetchRecipe(typeRecipe, id);
+    const resultObj = resultArray[typeFood][0];
+    console.log(resultObj);
+    setInformationFood({
+      image: resultObj.strDrinkThumb,
+      name: resultObj.strDrink,
+      category: resultObj.strCategory,
+      instructions: resultObj.strInstructions,
+      alcoholic: resultObj.strAlcoholic,
+    });
+    getIngredients(resultObj);
+  };
+
+  useEffect(() => {
+    console.log(url);
+
+    if (typeRecipe === 'themealdb') {
+      getInformationsMeals();
+    } else {
+      getInformationsDrinks();
+    }
+  }, []);
 
   function mostrarDados() {
-    console.log(history);
     console.log(inProgressRecipes);
     console.log(typeRecipe);
     console.log(id);
   }
 
-  const getInformationFoods = async () => {
-    
-  }
-
   return (
     <div>
-      {/* <img
-        src={  }
+      <img
+        src={ informationFood.image }
         alt="foto da receita finalizada"
         data-testid="recipe-photo"
-      /> */}
+      />
       <h1
         data-testid="recipe-title"
       >
-        Botar aqui o titulo da receita
+        { informationFood.name }
       </h1>
 
       <button
         type="button"
         data-testid="share-btn"
-        /* onClick={  } */
+        onClick={ () => linkCopied(url, setCopied) }
       >
         <img
           src={ shareIcon }
@@ -52,7 +116,7 @@ function RecipeInProgress() {
       <button
         type="button"
         data-testid="favorite-btn"
-        /* onClick={  } */
+      /* onClick={  } */
       >
         <img
           src={ whiteHeartIcon }
@@ -60,22 +124,32 @@ function RecipeInProgress() {
         />
       </button>
 
-      <h4 data-testid="recipe-category">Texto da cetegoria</h4>
+      {copied && <p>Link copied!</p>}
 
-      <p>Ingredients</p>
+      <h4 data-testid="recipe-category">{informationFood.category}</h4>
 
-      <li>
-        <input
-          /* data-testid={ `${ind}-ingredient-step` } */
-          type="checkbox"
-          name="mudarDepois"
-          value="mudarDepois"
-          /* onChange={ } */
-        />
-      </li>
+      <h5>Ingredients:</h5>
+
+      {
+        ingredients.map((ingredient, index) => (
+          <div
+            key={ index }
+            data-testid={ `${index}-ingredient-step` }
+            htmlFor={ index }
+          >
+            <input
+              data-testid={ `${index}-ingredient-step` }
+              id={ index }
+              type="checkbox"
+              /* onChange={ } */
+            />
+            { ingredient }
+          </div>
+        ))
+      }
 
       <p data-testid="instructions">
-        Botar aqui as instruções da receita
+        { informationFood.instructions }
       </p>
 
       <button
