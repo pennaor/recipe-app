147b9eeb5a9dbd4e0f5903aeb701d60786b1f9bb
jcaluiz <lacjunior129.la@gmail.com>
2022-08-27 12:35:28 -0300

    requisitos 44, 45, 46 e 47

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import shareIcon from '../images/shareIcon.svg';
 import { oneMeal, oneDrink } from '../tests/helpers/mockData/oneMealNDrinkDoneRecipe';
+import linkCopied from '../utils/linkCopied';
 
 export default function DoneRecipes() {
   const [cardsFinishedMock, setCardsFinishedMock] = useState([]);
+  const [copied, setCopied] = useState(false);
 
   useEffect(() => {
     setCardsFinishedMock([oneMeal, oneDrink]);
@@ -24,42 +26,76 @@ export default function DoneRecipes() {
       {
         cardsFinishedMock.map((card, index) => {
           keyCurr.push(Object.keys(card));
-          const verificateTags = card[keyCurr[index][0] === 'meals'
+          const mealOrDrink = keyCurr[index][0];
+          const verificateTags = card[mealOrDrink === 'meals'
             ? 'meals' : 'drinks'][0].strTags;
           // Estudando regex no site regexone e testando no regex101.
           // O match eu procurei na documentação sobre Regex, quais funções podem ser usada com regex
-          const tags = verificateTags && verificateTags.match(/\w*[^\W]/gm);
+          const TWO = 2;
+          const tags = verificateTags && verificateTags.match(/\w*[^\W]/gm)
+            .filter((_e, i) => i < TWO);
 
           return (
             <div
-              key={ keyCurr[index][0] === 'meals'
-                ? card[keyCurr[index][0]][0].strMealThumb
-                : card[keyCurr[index][0]][0].strDrinkThumb }
+              key={ mealOrDrink === 'meals'
+                ? card[mealOrDrink][0].strMealThumb
+                : card[mealOrDrink][0].strDrinkThumb }
             >
               <img
-                src={ keyCurr[index][0] === 'meals'
-                  ? card[keyCurr[index][0]][0].strMealThumb
-                  : card[keyCurr[index][0]][0].strDrinkThumb }
-                alt={ keyCurr[index][0] === 'meals'
-                  ? card[keyCurr[index][0]][0].strMeal
-                  : card[keyCurr[index][0]][0].strDrink }
+                src={ mealOrDrink === 'meals'
+                  ? card[mealOrDrink][0].strMealThumb
+                  : card[mealOrDrink][0].strDrinkThumb }
+                alt={ mealOrDrink === 'meals'
+                  ? card[mealOrDrink][0].strMeal
+                  : card[mealOrDrink][0].strDrink }
                 data-testid={ `${index}-horizontal-image` }
               />
 
-              <p data-testid={ `${index}-horizontal-top-text` }>{card.strCategory}</p>
-
               <p data-testid={ `${index}-horizontal-name` }>
-                { keyCurr[index][0] === 'meals'
-                  ? card[keyCurr[index][0]][0].strMeal
-                  : card[keyCurr[index][0]][0].strDrink }
+                { mealOrDrink === 'meals'
+                  ? card[mealOrDrink][0].strMeal
+                  : card[mealOrDrink][0].strDrink }
 
               </p>
 
-              <p data-testid={ `${index}-horizontal-done-date` }>08/25/2022</p>
+              {
+                mealOrDrink !== 'meals' && (
+                  <p
+                    data-testid={
+                      `${index}-horizontal-top-text`
+                    }
+                  >
+                    {card[mealOrDrink][0].strCategory}
+                  </p>
+                )
+              }
 
-              <button type="button" data-testid={ `${index}-horizontal-share-btn` }>
-                <img src={ shareIcon } alt="compartilhar" />
-              </button>
+              {
+                mealOrDrink === 'drinks' && (
+                  <p
+                    data-testid={
+                      `${index}-horizontal-top-text`
+                    }
+                  >
+                    {card[mealOrDrink][0].strAlcoholic}
+                  </p>
+                )
+              }
+
+              {
+                mealOrDrink === 'meals' && (
+                  <p
+                    data-testid={
+                      `${index}-horizontal-top-text`
+                    }
+                  >
+                    {`${card[mealOrDrink][0]
+                      .strArea} - ${card[mealOrDrink][0].strCategory} `}
+                  </p>
+                )
+              }
+              {/* data mockada esperando os requisitos da página anterior */}
+              <p data-testid={ `${index}-horizontal-done-date` }>23/06/2020</p>
 
               { tags && tags.map((tag, i) => (
                 <p
@@ -69,6 +105,23 @@ export default function DoneRecipes() {
                   {tag}
                 </p>
               ))}
+
+              <button
+                onClick={ () => (mealOrDrink === 'meals' ? (
+                  linkCopied(`/foods/${card[mealOrDrink][0].idMeal}`, setCopied)
+                ) : (
+                  linkCopied(`/drinks/${card[mealOrDrink][0].idDrink}`, setCopied)
+                )) }
+                type="button"
+              >
+                <img
+                  src={ shareIcon }
+                  alt="compartilhar"
+                  data-testid={ `${index}-horizontal-share-btn` }
+                />
+              </button>
+
+              {copied && <p style={ { display: 'block' } }>Link copied!</p>}
 
             </div>
           );
