8942c356288e6a39c48e6e88ae7884d2434fcd9e
Gabriel Vinícius <viniciusgsa99@gmail.com>
2022-08-29 18:59:48 -0300

    Requisitos 60, 61 e 62 concluídos. Adiciona funções dos botões, respectivamente, Done Recipes redireciona para page Done Ricpes, Favorite Recipes redireciona para page Favorite Recipes e Logout limpa todas as chaves do logal storage e redireciona para page de login.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,28 @@
 import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 export default function Profile() {
   const [email, setEmail] = useState('');
+  const { push } = useHistory();
+
   useEffect(() => {
-    setEmail(JSON.parse(localStorage.getItem('user')).email);
+    if (localStorage.getItem('user')) {
+      setEmail(JSON.parse(localStorage.getItem('user')).email);
+    }
   }, []);
+
+  const logout = () => {
+    localStorage.removeItem('mealsToken');
+    localStorage.removeItem('user');
+    localStorage.removeItem('cocktailsToken');
+    localStorage.removeItem('doneRecipes');
+    localStorage.removeItem('favoriteRecipes');
+    localStorage.removeItem('inProgressRecipes');
+    push('/');
+  };
+
   return (
     <div>
       <Header />
@@ -15,23 +31,30 @@ export default function Profile() {
           <button
             type="button"
             data-testid="profile-done-btn"
+            onClick={ () => push('/done-recipes') }
           >
             Done Recipes
           </button>
           <button
             type="button"
             data-testid="profile-favorite-btn"
+            onClick={ () => push('/favorite-recipes') }
           >
             Favorite Recipes
           </button>
           <button
             type="button"
             data-testid="profile-logout-btn"
+            onClick={ () => logout() }
           >
             Logout
           </button>
         </section>
-        <p data-testid="profile-email">{ email }</p>
+        <p data-testid="profile-email">
+          {
+            email.length > 0 ? email : 'Email'
+          }
+        </p>
       </main>
       <Footer />
     </div>
