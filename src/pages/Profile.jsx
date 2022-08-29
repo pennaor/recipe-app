2fbbf8db3b901ac4860c6c6ae345add666bbca80
Gabriel Vinícius <viniciusgsa99@gmail.com>
2022-08-29 17:38:06 -0300

    Requisito 57 concluído. Implementando botões na página profile.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -6,6 +6,29 @@ export default function Profile() {
   return (
     <div>
       <Header />
+      <main>
+        <section className="navigation">
+          <button
+            type="button"
+            data-testid="profile-done-btn"
+          >
+            Done Recipes
+          </button>
+          <button
+            type="button"
+            data-testid="profile-favorite-btn"
+          >
+            Favorite Recipes
+          </button>
+          <button
+            type="button"
+            data-testid="profile-logout-btn"
+          >
+            Logout
+          </button>
+        </section>
+        <p data-testid="profile-email">Email</p>
+      </main>
       <Footer />
     </div>
   );
